678e64552e476ea209cebc029afb878eb2ed039e
ahmad-teaching <ahmad.osman@digitalcareerinstitute.org>
2021-12-02 13:32:02 +0100

    task1

--- js-summerize/data_processing.js
+++ js-summerize/data_processing.js
@@ -1,5 +1,5 @@
 (async () => {
-    const URL = 'https://apif.worldbank.org/v2/country/?format=json&per_page=300';
+    const URL = 'https://api.worldbank.org/v2/country/?format=json&per_page=300';
     // print only the world regions data that you will get from the previous API 
     try {
         const response = await fetch(URL);
@@ -7,7 +7,7 @@
         console.log(data[1]);
         const dataSet = data[1];
         // task
-        // create an array that contains only the iso2code values from the items
+        // create an array that contains only the iso2Code values from the items
         // in the dataSet
 
     } catch (error) {
