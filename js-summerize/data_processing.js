54eb8cf08eaafefa03f01e0894c5322042ebc626
ahmad-teaching <ahmad.osman@digitalcareerinstitute.org>
2021-12-02 11:33:48 +0100

    get data precode

---
+++ js-summerize/data_processing.js
@@ -0,0 +1,10 @@
+async function getData() {
+    const URL = 'https://api.worldbank.org/v2/country/?format=json&per_page=300';
+// print the only the world regions data that you will get from the previous API 
+
+
+    console.log();
+}
+
+
+getData();
